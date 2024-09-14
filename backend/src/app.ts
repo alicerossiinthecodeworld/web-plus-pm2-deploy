6aa4a8b5fed92da4dfc5d830325a301292f00425
anproskuryakova <anproskuryakova@ozon.ru>
2024-09-14 17:58:40 +0500

    commit to deploy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,17 +4,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-// import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
 import routes from './routes';
 
+require('dotenv').config();
 const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
-// Только для локальных тестов. Не используйте это в продакшене
-// app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -22,5 +20,4 @@ app.use(routes);
 app.use(errors());
 app.use(errorHandler);
 
-// eslint-disable-next-line no-console
 app.listen(PORT, () => console.log('ok'));
