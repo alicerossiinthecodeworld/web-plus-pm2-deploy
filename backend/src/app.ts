2f1eaca95710f709e72965bdddef3883c5935b17
anproskuryakova <anproskuryakova@ozon.ru>
2024-12-04 23:05:54 +0500

    fixed api

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 mongoose.connect(DB_ADDRESS);
 
 app.use(cors({
-  origin: 'https://mestoalice.nomorepartiesco.ru',
+  origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
